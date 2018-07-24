e17838815c327460e46360b561ff05fbee6ca043
Abdul Noushad 2 <abdnoush@publicisgroupe.net>
2018-07-24 14:28:44 +0530

    Added patch(update) route for todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {ToDo} = require('./models/todo');
@@ -68,6 +69,34 @@ app.delete('/todos/:id', (req, res)=>{
   })
   .catch((err)=>{
     res.status(400).send(err.message);
+  });
+});
+
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  var body = _.pick(req.body, ['text','completed']);
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  ToDo.findByIdAndUpdate(id, {$set: body}, {new: true})
+  .then((todo)=> {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  })
+  .catch((e)=>{
+    res.status(400).send();
   });
 });
 
