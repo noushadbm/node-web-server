6c5cf520b9ff12c18a27ed356ae8cc66ee129869
Abdul Noushad 2 <abdnoush@publicisgroupe.net>
2018-07-24 11:50:59 +0530

    Added get toDo by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {ToDo} = require('./models/todo');
 var {User} = require('./models/user');
+var ObjectId = mongoose.Types.ObjectId;
 
 var port = process.env.PORT || 3000;
 
@@ -29,6 +30,24 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (err)=>{
     res.status(400).send({error: err.message});
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  ToDo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+        
+    res.send(todo);
+  }, (err)=>{
+    res.status(400).send(err.message);
   });
 });
 
