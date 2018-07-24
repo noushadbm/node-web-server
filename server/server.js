a1950460153e9812feebccca540b6ae9b1b210e1
Abdul Noushad 2 <abdnoush@publicisgroupe.net>
2018-07-24 12:15:51 +0530

    Added toDo delete route

--- server/server.js
+++ server/server.js
@@ -44,9 +44,29 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send();
     }
-        
+
     res.send(todo);
   }, (err)=>{
+    res.status(400).send(err.message);
+  });
+});
+
+app.delete('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  ToDo.findByIdAndRemove(id)
+  .then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  })
+  .catch((err)=>{
     res.status(400).send(err.message);
   });
 });
