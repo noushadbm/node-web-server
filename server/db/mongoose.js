2c473668d00c21da59b78cbf755e1a9fd1cc0417
Abdul Noushad 2 <abdnoush@publicisgroupe.net>
2018-07-26 17:31:27 +0530

    modifed db config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var mongoDbUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/app-testdb';
+var mongoDbUrl = process.env.MONGO_URL;
 console.log("mongo URL:" + mongoDbUrl);
 
 mongoose.connect(mongoDbUrl, {useNewUrlParser: true});
